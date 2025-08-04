41c8ab2bfa9a2d7c975aef3dbd00fa842f82fff1
Stephen Allen <Staylorallen@Gmail.com>
2025-08-04 17:57:14 -0400

    cleanup / setup for app

--- client/app/routes.ts
+++ client/app/routes.ts
@@ -1,3 +1,15 @@
-import { type RouteConfig, index } from "@react-router/dev/routes";
+import { type RouteConfig, index, route, layout } from "@react-router/dev/routes";
 
-export default [index("routes/_root/_root.tsx")] satisfies RouteConfig;
+export default [
+  layout("shared/layout/layoutApp.tsx", [
+    index("routes/_root/_root.tsx"),
+    route("login", "routes/auth/login/login.tsx"),
+    route("register", "routes/auth/register/register.tsx"),
+    layout("shared/layout/protectedRoutes.tsx", [
+      route("dashboard", "routes/dashboard/dashboard.tsx"),
+      route("profile", "routes/user/profile/profile.tsx"),
+    ]),
+    // Catch-all route for unmatched URLs (including well-known endpoints)
+    route("*", "routes/$.tsx"),
+  ]),
+] satisfies RouteConfig;
